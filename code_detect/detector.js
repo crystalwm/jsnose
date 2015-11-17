5e1eeaca29cbafc46ef41760e62ea6d05ec1da40
crystal <1207356200@qq.com>
2015-11-17 15:21:53 +0800

    源代码可以跑起来了

--- code_detect/detector.js
+++ code_detect/detector.js
@@ -1,11 +1,14 @@
-var nose = require('../lib/nose.js');
+var Nose = require('../lib/nose.js');
+var nose=new Nose({},
+    ['../_smells/css_in_javascript.js',
+    '../_smells/long_scope_chaining.js']);
 
 for(var i in nose){
     console.log(i);
 }
 
-
-/*
-nose.loadFiles('../_smells');
-nose.reporters('smell.txt');
-nose.run();*/
+nose.loadFiles(function(){
+    console.log("读取文件完毕！");
+});
+nose.reporter();
+nose.run();
